9aa90715610b36255da2a50ee915f9881e547c1f
lsmills24 <97650683+lsmills24@users.noreply.github.com>
2022-02-19 18:12:01 -0700

    Updated actor.py

---
+++ game/casting/actor.py
@@ -0,0 +1,64 @@
+from game.shared.color import Color
+from game.shared.point import Point
+
+class Actor: 
+    """A visible, moveable thing that participates in the game. 
+    
+    The responsibility of Actor is to keep track of its appearance, position and velocity in 2d 
+    space.
+
+    Attributes:
+        _text (string): The text to display
+        _font_size (int): The font size to use.
+        _color (Color): The color of the text.
+        _position (Point): The screen coordinates.
+        _velocity (Point): The speed and direction.
+    """
+
+    def __init__(self):
+        """Constructs new Actor"""
+        self._text = ""
+        self._font_size = 15
+        self._color = Color(255, 255, 255)
+        self._position = Point(0, 0)
+        self._velocity = Point(0, 0)
+
+    def get_color(self):
+        """Gets the actor's color as a tuple of three ints (r, g, b).
+        
+        Returns:
+            Color: The actor's text color.
+        """
+        return self._color
+
+    def get_font_size(self):
+        """Gets the actor's font size.
+        
+        Returns:
+            Point: The actor's font size.
+        """
+        return self._font_size
+
+    def get_position(self):
+        """Gets the actor's position in 2d space.
+        
+        Returns:
+            Point: The actor's position in 2d space.
+        """
+        return self._position
+
+    def get_text(self):
+        """Gets the actor's textual representation.
+        
+        Returns:
+            string: The actor's textual representation.
+        """
+        return self._text
+
+    def get_velocity(self):
+        """Gets the actor's speed and direction.
+        
+        Returns:
+            Point: The actor's speed and direction.
+        """
+        return self._velocity
