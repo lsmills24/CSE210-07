4c36dccb7ea3446e9c0751c35eeff462194fcbd8
ZannaMai <97138655+ZannaMai@users.noreply.github.com>
2022-02-23 12:03:19 -0700

    update main.py

--- __main__.py
+++ __main__.py
@@ -8,6 +8,7 @@ from game.directing.director import Director
 
 from game.services.earn_point import Point
 from game.services.lose_point import Lose
+from game.services.player_move import Player
 
 from game.shared.color import Color
 
@@ -25,6 +26,27 @@ DEFAULT_ARTIFACTS = 40
 
 def main():
     """main"""
+    for n in range(Gem, Rocks):
+        x = random.randint(1, COLS - 1)
+        y = random.randint(1, ROWS - 1)
+        position = Point(x, y)
+        position = position.scale(CELL_SIZE)
+
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        color = Color(r, g, b)
+
+        gem.set_color(color)
+        rock.set_color(color)
+        gem.set_position(position)
+        rock.set_position(position)
+        
+    
+
+
+director = Director(Player)
+director.start_game(Director)
 
 
 if __name__ == "__main__":
