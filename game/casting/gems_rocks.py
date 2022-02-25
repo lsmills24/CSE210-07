fdba9e3e05b771a0dabdf60217829cc53e55b196
ea0120 <97117717+ea0120@users.noreply.github.com>
2022-02-25 09:14:02 -0500

    added to gem and rock

--- game/casting/gems_rocks.py
+++ game/casting/gems_rocks.py
@@ -6,9 +6,53 @@ class Gem(Actor):
     """Responsible for random Gems appearing at the top
     and falling down.
     This class will pull from color.py for the random colors"""
+    def __init__(self):
+        super().__init__()
+        self._message = ""
+        
+    def get_message(self):
+        """Gets the gem's message.
+        
+        Returns:
+            string: The message.
+        """
+        return self._message
+    
+    def set_message(self, message):
+        """Updates the message to the given one.
+        
+        Args:
+            message (string): The given message.
+        """
+        self._message = message
+
 
 
 
 class Rocks(Actor):
     """Responsible for random rocks appearing at the top
     and falling down"""
+    def __init__(self):
+        super().__init__()
+        self._message = ""
+        
+    def get_message(self):
+        """Gets the rock's message.
+        
+        Returns:
+            string: The message.
+        """
+        return self._message
+    
+    def set_message(self, message):
+        """Updates the message to the given one.
+        
+        Args:
+            message (string): The given message.
+        """
+        self._message = message    
+        
+        
+        
+#drop down from top goes here
+class drop_down(Actor):
